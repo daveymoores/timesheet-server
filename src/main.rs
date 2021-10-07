3a4e2529079f2851a92bc51b571600e0cdd5c615
Davey Moores <daveymoores@gmail.com>
2021-10-07 16:46:49 +0200

    Rendering basic data with handlebars syntax

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use actix_files::{Files, NamedFile};
 use actix_web::http::StatusCode;
 use actix_web::{get, guard, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use dotenv::dotenv;
+use mongodb::bson::{self, doc, Bson, Document};
 use mongodb::options::{ClientOptions, ResolverConfig};
-use mongodb::{bson, bson::doc, bson::Bson};
 use std::path::{Path, PathBuf};
 
 use handlebars::Handlebars;
@@ -39,8 +39,7 @@ struct Timesheet {
 
 async fn find_record_from_random_string(
     path: &String,
-    hb: web::Data<Handlebars<'_>>,
-) -> Result<NamedFile, Box<dyn std::error::Error>> {
+) -> Result<Option<Document>, Box<dyn std::error::Error>> {
     dotenv().ok();
 
     // get the client uri
@@ -60,20 +59,43 @@ async fn find_record_from_random_string(
 
     let filter = doc! { "random_path": &path };
 
-    let document = collection.find_one(filter, None).await?;
+    let document: Option<Document> = collection.find_one(filter, None).await?;
+
+    Ok(document)
+}
+
+#[derive(Deserialize)]
+struct Info {
+    identifier: String,
+}
+
+// catch only routes that don't end with .* so that the assets don't
+// get resolved by this route and 404
+#[get("/{identifier:\\w+$}")]
+async fn timesheet(info: web::Path<Info>, hb: web::Data<Handlebars<'_>>) -> HttpResponse {
+    println!("info: {:#?}", info.identifier);
+    let document = find_record_from_random_string(&info.identifier)
+        .await
+        .expect("Failed to fetch record from mongodb");
 
     match document {
         None => {
-            let path: PathBuf = Path::new(env!("CARGO_MANIFEST_DIR")).join("dist/404.html");
-            Ok(NamedFile::open(path)?.set_status_code(StatusCode::NOT_FOUND))
+            let data = json!({
+                "error": "error",
+                "status_code": 401
+            });
+            let body = hb.render("404", &data).unwrap();
+            HttpResponse::Ok().body(body)
         }
         Some(doc) => {
-            let sheet: Timesheet = bson::from_bson(Bson::Document(doc))?;
-            let timesheet_json: Map<String, Value> = serde_json::from_str(&sheet.timesheet)?;
+            let sheet: Timesheet = bson::from_bson(Bson::Document(doc))
+                .expect("Couldn't parse Timesheet struct from document");
+            let timesheet_json: Map<String, Value> = serde_json::from_str(&sheet.timesheet)
+                .expect("Couldn't parse timesheet json from document");
 
             let data = json!({
                 "id": sheet.id,
-                "creation_date": sheet.creation_date,
+                "creation_date": sheet.creation_date.date().format("%B").to_string(),
                 "random_path": sheet.random_path,
                 "name": sheet.name,
                 "email": sheet.email,
@@ -85,33 +107,10 @@ async fn find_record_from_random_string(
                 "timesheet": timesheet_json,
             });
 
-            println!("{:#?}", data);
-
-            // let body = hb.render("timesheet", &data).unwrap();
-
-            let path: PathBuf = Path::new(env!("CARGO_MANIFEST_DIR")).join("dist/timesheet.html");
-            Ok(NamedFile::open(path)?)
+            let body = hb.render("timesheet", &data).unwrap();
+            HttpResponse::Ok().body(body)
         }
     }
-}
-
-#[derive(Deserialize)]
-struct Info {
-    identifier: String,
-}
-
-// catch only routes that don't end with .* so that the assets don't
-// get resolved by this route and 404
-#[get("/{identifier:\\w+$}")]
-async fn timesheet(
-    info: web::Path<Info>,
-    hb: web::Data<Handlebars<'_>>,
-) -> Result<NamedFile, Error> {
-    println!("info: {:#?}", info.identifier);
-    let named_file = find_record_from_random_string(&info.identifier, hb)
-        .await
-        .expect("Failed to fetch record from mongodb");
-    Ok(named_file)
 }
 
 async fn p404() -> Result<NamedFile, Error> {
@@ -127,7 +126,7 @@ async fn main() -> std::io::Result<()> {
 
     let mut handlebars = Handlebars::new();
     handlebars
-        .register_templates_directory(".html", "./static/templates")
+        .register_templates_directory(".html", "./dist")
         .unwrap();
     let handlebars_ref = web::Data::new(handlebars);
 
